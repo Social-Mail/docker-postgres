d817d125f9aefeb4fb25f2b7c0302b90da63175c
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-11-12 18:56:14 +0530

    pg-ssl fixed

--- pg-ssl/entry.sh
+++ pg-ssl/entry.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-/app/script.sh
+echo "Starting up"
+
+exec ./script.sh
 
 crond -l 0 -f -c /etc/crontabs
--- pg-ssl/script.sh
+++ pg-ssl/script.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
-if [ "$CERTBOT_ENV" = "staging" ]; then
+if [ "$CERTBOT_ENV" == "staging" ]; then
 
-    certbot \
-        certonly \
-        --staging \
-        --cert-name \
-        $CERT_NAME \
-        -d \
-        $DOMAIN_NAME \
-        --dns-route53 \
-        --non-interactive \
-        --agree-tos \    
+certbot \
+    certonly \
+    --staging \
+    --cert-name \
+    $CERT_NAME \
+    -d \
+    $DOMAIN_NAME \
+    --dns-route53 \
+    --non-interactive \
+    --agree-tos \
 
 else
-    certbot \
-        certonly \
-        --cert-name \
-        $CERT_NAME \
-        -d \
-        $DOMAIN_NAME \
-        --dns-route53 \
-        --non-interactive \
-        --agree-tos \    
+
+certbot \
+    certonly \
+    --cert-name \
+    $CERT_NAME \
+    -d \
+    $DOMAIN_NAME \
+    --dns-route53 \
+    --non-interactive \
+    --agree-tos \
+
 
 fi
