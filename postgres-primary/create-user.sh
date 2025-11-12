877cb1ab63b1a28046abf77c7f27d56a45b06edc
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-11-12 21:10:20 +0530

    wip

--- postgres-primary/create-user.sh
+++ postgres-primary/create-user.sh
@@ -5,5 +5,3 @@ set -e
 psql -v ON_ERROR_STOP=1 <<-EOSQL
     CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD '$PG_REPLICATOR_PASSWORD';
 EOSQL
-
-echo "host all replicator 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
--- postgres-replica/entrypoint.sh
+++ postgres-replica/entrypoint.sh
@@ -6,6 +6,8 @@ set -e
 chown -R postgres:postgres "$PGDATA"
 chmod 700 "$PGDATA"
 
+export PGPASSWORD="$PG_REPLICATOR_PASSWORD"
+
 # Wait until master is ready
 echo "Waiting for master to be ready..."
 until pg_isready -h $PG_MASTER -p 5432 -U $PG_REPLICATOR_USER; do
@@ -16,9 +18,14 @@ done
 # Initialize replica if needed
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
     echo "Running base backup..."
-    export PGPASSWORD="$PG_REPLICATOR_PASSWORD"
     gosu postgres pg_basebackup -h $PG_MASTER -U $PG_REPLICATOR_USER -D "$PGDATA" -Fp -Xs -P -R
     chmod 700 "$PGDATA"
 fi
+
+cp -f $SSL_CERT_FILE /var/lib/postgresql/server.crt
+cp -f $SSL_KEY_FILE /var/lib/postgresql/server.key
+
+chmod 600 /var/lib/postgresql/server.*
+chown postgres:postgres /var/lib/postgresql/server.*
 
 docker-entrypoint.sh "$@"
