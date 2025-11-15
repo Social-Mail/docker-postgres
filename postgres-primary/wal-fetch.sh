3c988bf76bcd272d015b96b7e87c26927a502363
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-11-15 20:52:08 +0530

    added set-e

--- postgres-primary/wal-fetch.sh
+++ postgres-primary/wal-fetch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 aws s3 cp --sse-c AES256 --sse-c-key fileb:///app/.pwd-hash "s3://$PG_BACKUP_STORAGE_S3_BUCKET/$PG_BACKUP_STORAGE_S3_WAL_FOLDER/$1.gz" "$2.gz"
 gunzip -c "$2.gz" > $2
 rm "$2.gz"
