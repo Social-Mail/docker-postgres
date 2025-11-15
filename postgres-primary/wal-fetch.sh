a7d25280ebb823c75d3b5c81cf2574c408159b0f
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-11-15 17:37:39 +0530

    Slash fixed

--- postgres-primary/wal-fetch.sh
+++ postgres-primary/wal-fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-aws s3 cp --sse-c-copy-source AES256 --sse-c-copy-source-key fileb://app/.pwd-hash "s3://$PG_BACKUP_STORAGE_S3_BUCKET/$PG_BACKUP_STORAGE_S3_WAL_FOLDER/$1.gz" "$2.gz"
+aws s3 cp --sse-c-copy-source AES256 --sse-c-copy-source-key fileb:///app/.pwd-hash "s3://$PG_BACKUP_STORAGE_S3_BUCKET/$PG_BACKUP_STORAGE_S3_WAL_FOLDER/$1.gz" "$2.gz"
 gunzip -c "$2.gz" > $2
 rm "$2.gz"
--- postgres-primary/wal-send.sh
+++ postgres-primary/wal-send.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 gzip -c "$1" > "$1.gz"
-aws s3 cp --sse-c AES256 --sse-c-key fileb://app/.pwd-hash "$1.gz" "s3://$PG_BACKUP_STORAGE_S3_BUCKET/$PG_BACKUP_STORAGE_S3_WAL_FOLDER/$2.gz"
+aws s3 cp --sse-c AES256 --sse-c-key fileb:///app/.pwd-hash "$1.gz" "s3://$PG_BACKUP_STORAGE_S3_BUCKET/$PG_BACKUP_STORAGE_S3_WAL_FOLDER/$2.gz"
 rm "$1.gz"
