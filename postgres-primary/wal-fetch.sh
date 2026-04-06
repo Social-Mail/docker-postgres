b4b1309b8e72506b6313193d2766999ed661240d
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-04-06 11:49:05 +0530

    added postgis

--- postgres-primary/wal-fetch.sh
+++ postgres-primary/wal-fetch.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-aws s3 cp "s3://$PG_BACKUP_STORAGE_S3_BUCKET/$PG_BACKUP_STORAGE_S3_WAL_FOLDER/$1.gz.enc" "$2.gz.enc"
-openssl enc -d -aes-256-cbc -pbkdf2 -in "$2.gz.enc" -out "$2.gz" -pass file:/app/.pwd-hash
-gunzip -c "$2.gz" > $2
-rm "$2.gz.enc"
-rm "$2.gz"
+if [ "$PG_TEST" == "test" ]; then
+   echo "test wal-fetch $1 < $2"
+else
+    aws s3 cp "s3://$PG_BACKUP_STORAGE_S3_BUCKET/$PG_BACKUP_STORAGE_S3_WAL_FOLDER/$1.gz.enc" "$2.gz.enc"
+    openssl enc -d -aes-256-cbc -pbkdf2 -in "$2.gz.enc" -out "$2.gz" -pass file:/app/.pwd-hash
+    gunzip -c "$2.gz" > $2
+    rm "$2.gz.enc"
+    rm "$2.gz"
+fi
--- postgres-primary/wal-send.sh
+++ postgres-primary/wal-send.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-gzip -c "$1" > "$1.gz"
-openssl enc -aes-256-cbc -pbkdf2 -in "$1.gz" -out "$1.gz.enc" -pass file:/app/.pwd-hash
-aws s3 cp "$1.gz.enc" "s3://$PG_BACKUP_STORAGE_S3_BUCKET/$PG_BACKUP_STORAGE_S3_WAL_FOLDER/$2.gz.enc"
-rm "$1.gz.enc"
-rm "$1.gz"
+if [ "$PG_TEST" == "test" ]; then
+   echo "test wal-send $1 > $2"
+else
+    gzip -c "$1" > "$1.gz"
+    openssl enc -aes-256-cbc -pbkdf2 -in "$1.gz" -out "$1.gz.enc" -pass file:/app/.pwd-hash
+    aws s3 cp "$1.gz.enc" "s3://$PG_BACKUP_STORAGE_S3_BUCKET/$PG_BACKUP_STORAGE_S3_WAL_FOLDER/$2.gz.enc"
+    rm "$1.gz.enc"
+    rm "$1.gz"
+fi
